c46df97579e6066e3711d04d45f581a3f574ae71
dhuziran <danielhuziran@gmail.com>
2023-06-11 23:17:19 -0500

    final commit

--- puppy-bowl/src/components/players/Players.jsx
+++ puppy-bowl/src/components/players/Players.jsx
@@ -1,18 +1,42 @@
 import './Players.css'
-import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Players({puppies}){
-    
+    const [searchQuery, setSearchQuery] = useState('')
+
+    let filteredPuppies = puppies.filter((puppy) => {
+        let lowercasedPuppy = puppy.name.toLowerCase()
+        let lowercasedSearch = searchQuery.toLowerCase()
+
+        if(lowercasedPuppy.includes(lowercasedSearch)){
+            return puppy
+        }
+    })
     return (
+
         <div className='players-container'>
+
+            <form>
+                <label htmlFor='search-query'>Search For Puppy</label>
+                <input
+                name='search-query'
+                type='text'
+                placeholder='Puppy Name'
+                value={searchQuery}
+                onChange={(e) => {
+                    setSearchQuery(e.target.value)
+                }}></input>
+            </form>
+
+
             <h1 className='header2'>ALL PLAYERS PARTICIPATING</h1>
             <div className='players'>
-                {puppies.map((el) => {
+                {filteredPuppies.map((el) => {
                     
                     return (
                         <div className='individual-player' key = {el.id}>
                             <h2 >{el.name}</h2>
-                            <img className='image' src = {el.imageUrl} alt = {`Picture of ${el.name}`} />
                             <Link to = {`/players/${el.id}`}><button className='puppy-button'> See More Details </button></Link>
                         </div>
                     )
